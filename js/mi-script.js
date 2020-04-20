7ef0d45ba25ffc9118f73189adb0b127da31646d
Laura Moreno Valencia <laura962310@gmail.com>
2020-04-20 13:27:25 -0500

    Clase 20 abril

---
+++ js/mi-script.js
@@ -0,0 +1,24 @@
+//let numero1= 8
+//let numero2= 6
+
+//const suma=numero1+numero2
+
+//console.log ("La suma es: " + suma)
+
+//console.log(4<9)//true, false (4 menor que 9)
+//console.log(4==4)//true, false (4 igual a 4)
+//console.log(4!=3)//true,false (4 diferente que 3)
+
+//if (4=4){ console.log ("La condicion se cumple");}
+//if(numero1>numero2){ console.log("La condicion se cumple");}else{console.log("La condicion no se cumple");}
+
+
+let numero1 = prompt("Digite número 1");
+let numero2 = prompt("Digite número 2");
+const suma = numero1 + numero2;
+
+if(numero1 >= numero2){
+    console.log(numero1," es mayor o igual a ", numero2);
+}else{
+    console.log(numero1,  " no es mayor ni igual a ",  numero2);
+}
